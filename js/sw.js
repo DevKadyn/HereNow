acb0ee231281f7b0e46a0802bf474ef908bb2aa6
DevKadyn <74390402+DevKadyn@users.noreply.github.com>
2020-11-23 23:23:16 -0600

    Minor Update to Release 1.0
    Adding in Manifest, App and SW into GitHub to allow for app to be installed.

---
+++ js/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('pwa-example').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.js',
+                '/css/style.css',
+                '/js/weatherApp.js'//Add any other assets your web page needs
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
